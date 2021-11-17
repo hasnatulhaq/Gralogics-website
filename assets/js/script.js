09e2ba40299684e3d507d39cd1719d27d1a5ebfa
Hasnat ul haq <hasnatulhaq@outlook.com>
2021-11-17 18:59:35 +0500

    naming css classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
-const mymap = L.map('assetMap').setView([36.49082 , -119.7119], 10);
-			const attribution = 'Rendered by AGRALOGICS using ESRI & GeoEye Basemaps';
-			const tileUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-			//'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-			const tiles = L.tileLayer(tileUrl, {attribution});
-			tiles.addTo(mymap);
-			//commit 
+const mymap = L.map('mymap').setView([36.49082 , -119.7119], 10);
+const attribution = 'Rendered by AGRALOGICS using ESRI & GeoEye Basemaps';
+const tileUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+//'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, {attribution});
+tiles.addTo(mymap);
