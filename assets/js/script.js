1b842f3e55d5dc6e0477e754e978f0fa6dbde405
Shahzad Bacha <shahzadbacha.gis@gmail.com>
2021-11-20 00:28:11 +0500

    integrating apis

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,52 @@ const tileUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imag
 //'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const tiles = L.tileLayer(tileUrl, {attribution});
 tiles.addTo(mymap);
+
+$(function() {
+    $("#searchBox").autocomplete({
+        source : function(request, response) {
+            $.ajax({
+                type: "GET",
+                url : "http://localhost:5000/autocomplete",
+                dataType : "json",
+                cache: false,
+                data: {
+                    input: searchBox.value
+                },
+                success: function(data) {
+                    response(data);
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    console.log(textStatus + " " + errorThrown);
+                },
+            });
+        },
+        select: function( event, ui ) {
+            $.ajax({
+                type: "GET",
+                url: "http://localhost:5000/parcels",
+                dataType: 'json',
+                data: {
+                    apn: ui.item.value
+                },
+                success: function (response) {
+                    var feature = L.geoJson(response)
+                    mymap.fitBounds(feature.getBounds());
+                }
+            });
+        },
+        minLength : 2
+    });
+    
+});
+
+$.ajax({
+    type: "GET",
+    url: "http://localhost:5000/parcels",
+    dataType: 'json',
+    success: function (response) {
+        geojsonLayer = L.geoJson(response, {
+            // style: yourLeafletStyle
+        }).addTo(mymap)
+    }
+});
